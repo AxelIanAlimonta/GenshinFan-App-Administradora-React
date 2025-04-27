15fcdff60591f1f07f08e0ad9b3b8a23b8533c9b
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-04-27 20:31:29 -0300

    Update AgregarRegion.jsx

--- src/pages/Regiones/AgregarRegion/AgregarRegion.jsx
+++ src/pages/Regiones/AgregarRegion/AgregarRegion.jsx
@@ -30,25 +30,26 @@ export default function AgregarRegion() {
     return (
         <>
             <Header />
-                <Form className="formularioAgregarRegion">
-                    <Form.Group controlId="formDescripcion">
-                        <Form.Label>Descripción</Form.Label>
-                        <Form.Control type="text" placeholder="Descripción" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
-                    </Form.Group>
-                    <Form.Group controlId="formImagenURL">
-                        <Form.Label>URL de la Imagen</Form.Label>
-                        <Form.Control type="text" placeholder="URL" value={imagenURL} onChange={(e) => setImagenURL(e.target.value)} />
-                        <PreviaDeImagen imagenURL={imagenURL} />
-                    </Form.Group>
-                    <div className="formularioAgregarRegion-botones">
-                        <Button variant="danger" onClick={handleCancelar}>
-                            Cancelar
-                        </Button>
-                        <Button variant="success" onClick={handleAgregarRegion}>
-                            Agregar Región
-                        </Button>
-                    </div>
-                </Form>
+            <Form className="formularioAgregarRegion">
+                <Form.Group controlId="formDescripcion">
+                    <Form.Label>Descripción</Form.Label>
+                    <Form.Control type="text" placeholder="Descripción" value={descripcion} onChange={(e) => setDescripcion(e.target.value)} />
+                </Form.Group>
+                <Form.Group controlId="formImagenURL">
+                    <Form.Label>URL de la Imagen</Form.Label>
+                    <Form.Control type="text" placeholder="URL" value={imagenURL} onChange={(e) => setImagenURL(e.target.value)} />
+                    <PreviaDeImagen imagenURL={imagenURL} />
+                </Form.Group>
+                <div className="formularioAgregarRegion-botones">
+                    <Button variant="success" onClick={handleAgregarRegion}>
+                        Agregar Región
+                    </Button>
+                    <Button variant="danger" onClick={handleCancelar}>
+                        Cancelar
+                    </Button>
+
+                </div>
+            </Form>
 
         </>
     );
