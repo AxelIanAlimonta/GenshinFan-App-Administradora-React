1f220d1f0146de0479b095e65f97769a115eb7da
Axel Ian Alimonta <axelalimonta@gmail.com>
2025-07-12 03:18:25 -0300

    Update image preview styling and formatting
    Changed .img-preview CSS to use fixed height instead of max-width/max-height.

--- src/components/PreviaDeImagen/PreviaDeImagen.jsx
+++ src/components/PreviaDeImagen/PreviaDeImagen.jsx
@@ -3,8 +3,8 @@ import './PreviaDeImagen.css';
 export default function PreviaDeImagen({ imagenURL }) {
     return (<>
         {imagenURL && (
-            <img src={imagenURL} alt="Vista previa de la imagen" className="img-preview"
-            />
+                <img src={imagenURL} alt="Vista previa de la imagen" className="img-preview"
+                />
         )
         }</>
 
